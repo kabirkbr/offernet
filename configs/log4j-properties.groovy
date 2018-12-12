30a1b0cd6f83e632c50608dbf82a2494be940129
kabirkbr <kabir@singularitynet.io>
2018-12-12 18:17:16 +0100

    added rolling file appender for debug file: 10 files , 10 mb limits

--- configs/log4j-properties.groovy
+++ configs/log4j-properties.groovy
@@ -1,4 +1,4 @@
-log4j.rootLogger = "INFO, SIM"
+log4j.rootLogger = "DEBUG, SIM, DB"
 
 log4j.appender.SIM = "org.apache.log4j.RollingFileAppender"
 log4j.appender."SIM.File" = './logs/OfferNet.log'
@@ -9,8 +9,10 @@ log4j.appender."SIM.layout"="org.apache.log4j.PatternLayout"
 log4j.appender."SIM.layout.ConversionPattern"= '%d{ISO8601} [%t] %-5p %c %x : %m%n'
 log4j.appender."SIM.Threshold"="INFO"
 
-log4j.appender.DB = "org.apache.log4j.FileAppender"
+log4j.appender.DB = "org.apache.log4j.RollingFileAppender"
 log4j.appender."DB.File" = './logs/OfferNetVerbose.log'
+log4j.appender."DB.MaxFileSize"="10GB"
+log4j.appender."DB.MaxBackupIndex"=10
 log4j.appender."DB.Append"=true
 log4j.appender."DB.layout"="org.apache.log4j.PatternLayout"
 log4j.appender."DB.layout.ConversionPattern"= '%d{ISO8601} [%t] %-5p %c %x : %m%n'
